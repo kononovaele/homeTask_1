ac0cf1b445af2daada7bf4ce88889c617ef6e264
Kononovaele <Kononovaele@yandex.ru>
2023-01-01 20:37:56 +0300

    add python files

---
+++ main.py
@@ -0,0 +1,8 @@
+import sum_array
+
+def main():
+    my_array = [1, 5, 11, 7, 5, 2]
+    print(sum_array(my_array))
+
+main()
+
---
+++ sum_array.py
@@ -0,0 +1,5 @@
+
+def sum_array(my_array):
+    sum = 0
+
+    return sum
