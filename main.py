2a0f898fe463c3a062ab79788f53e19496c2f894
Kononovaele <Kononovaele@yandex.ru>
2023-01-01 20:43:59 +0300

    change in print

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sum_array
+import func_array
 
 def main():
     my_array = [1, 5, 11, 7, 5, 2]
-    print(sum_array(my_array))
+    print('Sum array:', func_array.sum_array(my_array))
 
 main()
 
