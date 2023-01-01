ab071afcc5758a05a4411c3fa2890dae36226748
Kononovaele <Kononovaele@yandex.ru>
2023-01-01 22:27:14 +0300

    end project

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 import func_array
 
 def main():
+
+    print('Starting...')
+
     my_array = [1, 4, 0, 0, 99, 3]
-    print('sum all elemets of array', func_array.sum_array(my_array))
+    print('sum all elemets of array:', func_array.sum_array(my_array))
 
 main()
